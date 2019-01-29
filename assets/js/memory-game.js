ed9ac6a3603356356db6aa7635a93015c9053f78
Deirdre van der Zee <deirdrevanderzee@gmail.com>
2019-01-29 13:56:47 +0000

    Shuffle Cards on Gameboard

--- assets/js/memory-game.js
+++ assets/js/memory-game.js
@@ -9,5 +9,16 @@ var memoryCards = [...memoryCard];
 
 //Loop through the cards and add an event listener to each card
 for (var i = 0; i < memoryCards.length; i++){
-   memoryCards[i].addEventListener('click', rotateCard);
+    memoryCards[i].addEventListener('click', rotateCard);
 };
+
+//Shuffle the cards on the Gameboard - remove the rotateCard class so cards are reset
+function shuffleGameboard() {
+    for (var i = 0; i < memoryCards.length; i++){
+        var randomPosition = Math.floor(Math.random() * 12);
+        memoryCard[i].style.order = randomPosition;
+        memoryCard[i].classList.remove('rotateCard');        
+    };
+};
+
+document.body.onload = shuffleGameboard();
