eb452ef816dd1af0c54a3fd32c9d2fba72ebf4ca
Deirdre van der Zee <deirdrevanderzee@gmail.com>
2019-02-07 16:00:49 +0000

    Added Levels and tidied styling

--- assets/js/memory-game.js
+++ assets/js/memory-game.js
@@ -111,7 +111,6 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
-    
 };
 
 // When the user clicks anywhere outside of the modal, close it
